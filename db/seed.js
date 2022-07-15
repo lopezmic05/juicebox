9609f0ac54c0c671bc531011593ceea405e12804
Michael Lopez <michael.94lopez@gmail.com>
2022-07-15 11:11:54 -0500

    initializing seeding

--- db/seed.js
+++ db/seed.js
@@ -1,22 +1,69 @@
-// grab our client with destructuring from the export in index.js
-const { client } = require('./index');
+const {
+  client,
+  getAllUsers
+} = require('./index');
 
-async function testDB() {
+async function dropTables() {
   try {
-    // connect the client to the database, finally
-    client.connect();
+    console.log("Starting to drop tables...");
 
-    // queries are promises, so we can await them
-    const { rows } = await client.query(`SELECT * FROM users;`);
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
 
-    // for now, logging is a fine way to see what's up
-    console.log(rows);
+    console.log("Finished dropping tables!");
   } catch (error) {
-    console.error(error);
-  } finally {
-    // it's important to close out the client connection
-    client.end();
+    console.error("Error dropping tables!");
+    throw error;
   }
 }
 
-testDB();
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+      CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username varchar(255) UNIQUE NOT NULL,
+        password varchar(255) NOT NULL
+      );
+    `);
+
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error;
+  }
+}
+
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    const users = await getAllUsers();
+    console.log("getAllUsers:", users);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
+
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
